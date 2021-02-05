3b682389bb0d34ad91925f9768833ccd5267fb03
Jesse <moore.jesse@gmail.com>
2021-02-05 10:19:38 -0600

    Exercises for Chapter 8

--- src/org/launchcode/java/demos/lsn7interfaces/Cone.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Cone.java
@@ -9,4 +9,6 @@ public class Cone extends Ingredient {
     }
 
 
+
+
 }
--- src/org/launchcode/java/demos/lsn7interfaces/Main.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Main.java
@@ -11,9 +11,15 @@ public class Main {
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name'
         //  field.
 
+        flavors.sort(new FlavorComparator());
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
         //  field.
 
+        cones.sort(new ConeComparator());
+
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+        menu.getFlavors().forEach(flavor -> System.out.println(flavor.getName() + ": " + flavor.getAllergens()));
+        System.out.println("\r");
+        menu.getCones().forEach(cone -> System.out.println(cone.getName() + ": " + cone.getCost()));
     }
 }
